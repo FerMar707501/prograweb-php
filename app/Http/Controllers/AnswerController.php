e58a55211770faffcf3f63d2678b058ea14a80fa
FerMar707501 <ferlopez_16@umes.edu.gt>
2025-10-22 20:33:07 -0600

    He comentado 1 archivo

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -8,7 +8,10 @@ use App\Models\Question;
 class AnswerController extends Controller
 {
     public function store(Request $request, Question $question){
-        
+        /**
+         * Validate the incoming request data
+         * AQUI HEMOS VALIDADO QUE EL CAMPO CONTENT ES REQUERIDO, DE TIPO STRING Y CON UN MAXIMO DE 1900 CARACTERES
+         */
         $request->validate([
             'content' => 'required|string|max:1900',
         ]);
