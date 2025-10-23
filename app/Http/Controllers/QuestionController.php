6474bd5a0111b36648ad719810dbc15ef506cdc4
FerMar707501 <ferlopez_16@umes.edu.gt>
2025-10-22 20:27:46 -0600

    Codigo base de laravel

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Question;
+
+class QuestionController extends Controller
+{
+    public function show(Question $question){
+
+        $question->load('answers','category','user');
+
+        return view('questions.show',[
+            'question' => $question,
+        ]);
+    }
+
+    public function destroy(Question $question){
+        $question->delete();
+        return redirect()->route('home');
+    }
+}
+
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php   
+
+use App\Livewire\Settings\Appearance;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Profile;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AnswerController;
+
+Route::get('/',[PageController::class,'index'])->name('home');
+Route::get('/question/{question}',[QuestionController::class,'show'])->name('question.show');
+Route::post('/answers/{question}',[AnswerController::class,'store'])->name('answers.store');
+Route::delete('/questions/{question}',[QuestionController::class,'destroy'])->name('questions.destroy');
+
+
+
+
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Route::get('settings/profile', Profile::class)->name('settings.profile');
+    Route::get('settings/password', Password::class)->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+});
+
+require __DIR__.'/auth.php';
